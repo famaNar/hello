be823ce73fb3c351d38c674255f9bcff2b05464a
famaNar <famaamar@esp.sn>
2024-04-19 19:11:56 +0000

    Votre message de commit ici

---
+++ Desktop/Sb-app-0/src/main/java/esp/dgi/carshop/Sbapp0/SbApp0Application.java
@@ -0,0 +1,15 @@
+package esp.dgi.carshop.Sbapp0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbApp0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbApp0Application.class, args);
+		System.out.println("HELLO!");
+		System.out.println("I love Spring !");
+	}
+
+}
---
+++ Desktop/Sb-app-0/src/test/java/esp/dgi/carshop/Sbapp0/SbApp0ApplicationTests.java
@@ -0,0 +1,13 @@
+package esp.dgi.carshop.Sbapp0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbApp0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
