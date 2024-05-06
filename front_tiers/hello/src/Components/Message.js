668ac25a5dbe72f98e82163f54b0a97cb078f4e7
famaNar <127701410+famaNar@users.noreply.github.com>
2024-05-06 11:06:43 +0000

    Update Message.js

--- front_tiers/hello/src/Components/Message.js
+++ front_tiers/hello/src/Components/Message.js
@@ -1,10 +1,18 @@
 function Message({name, job, age}) {
-    return <h2>This is a message from fama AMAR</h2>;
+    return (
     <p className="alert alert-success">
-        This is message from <b>{name}</b>
+        This is message from <b>{name}</b> who is a <b>{job}, and he is {""}</b>
+        <b>{age}</b> years old.
 
     </p>
-
+)
     
   }
-  export default Message;
+  
+  /* function Message(props){
+    return (
+        <p  className="bg-primary  text-while display-5">
+            This is a message from <b>{props.name}</b> who is a <b>{props.job}  </b>and <b>{props.age}</b>
+        </p>
+    )
+}*/ export default Message;
