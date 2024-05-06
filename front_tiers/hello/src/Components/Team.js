3c2ec2a3fd883e3674af03c26a89f3287bf6b278
famaNar <127701410+famaNar@users.noreply.github.com>
2024-05-06 11:06:03 +0000

    Update Team.js

---
+++ front_tiers/hello/src/Components/Team.js
@@ -0,0 +1,8 @@
+function Team({name, title}){
+    return (
+        <p className="alert alert-success">
+            <b>{name}</b> a remporté <b>{title}</b> titres
+      </p>
+    )
+}export default Team;
+
